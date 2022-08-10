9c526b344687e8774be6d8605d3091348c67b64d
msk_0 <msk_0@DESKTOP-LTIAP7E>
2022-08-10 13:48:26 +0900

    test not mapping

---
+++ eclipesshortkey/src/eclipesshortkey_rename/EclipseShorkey_Rename.java
@@ -0,0 +1,13 @@
+package eclipesshortkey_rename;
+
+import java.util.Arrays;
+
+public class EclipseShorkey_Rename {
+
+	public static void main(String[] args) {
+		String str = "Hello";
+		Number n;
+		Arrays.toString(new int[] { 1, 2, 3 });
+		System.out.println("¡");
+	}
+}
