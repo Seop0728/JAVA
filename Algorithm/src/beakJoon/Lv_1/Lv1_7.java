aff33d4f1032eea2d910e01090b5b64b3c058f6a
msk_0 <msk_0@DESKTOP-LTIAP7E>
2022-08-10 14:58:38 +0900

    algorithm

---
+++ Algorithm/src/beakJoon/Lv_1/Lv1_7.java
@@ -0,0 +1,14 @@
+package beakJoon.Lv_1;
+import java.util.Scanner;
+
+//첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
+public class Lv1_7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println(a +"??!");
+		
+	}
+
+}
