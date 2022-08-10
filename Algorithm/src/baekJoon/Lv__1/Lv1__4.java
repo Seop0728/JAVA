c7d78a6307a587adbc5c5b083dde0edda02cc60d
msk_0 <msk_0@DESKTOP-LTIAP7E>
2022-08-10 13:01:52 +0900

    test

---
+++ Algorithm/src/baekJoon/Lv__1/Lv1__4.java
@@ -0,0 +1,18 @@
+package baekJoon.Lv__1;
+import java.util.Scanner;
+
+//첫째 줄에 A-B를 출력한다.
+
+
+public class Lv1__4 {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			System.out.println(a - b);
+
+	}
+
+}
