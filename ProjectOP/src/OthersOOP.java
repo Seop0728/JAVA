77f8299ff16cc37a6ac404d9c5a985b37b000700
msk_0 <msk_0@DESKTOP-LTIAP7E>
2022-08-11 12:10:58 +0900

    OOP

---
+++ ProjectOP/src/OthersOOP.java
@@ -0,0 +1,28 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class OthersOOP {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		// class : System, Math, FileWriter
+		// F1,F2
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.2));
+		System.out.println(Math.ceil(1.2));
+
+		FileWriter f1 = new  FileWriter("data.txt");
+		f1.write("hello");
+		f1.write("java");
+		f1.close();
+		
+		FileWriter f2 = new FileWriter("data2.txt");
+		f2.write("hello2");
+		f2.write("java2");
+		f2.close();
+		
+		
+	}
+
+}
---
+++ ProjectOP/src/Print.java
@@ -0,0 +1,17 @@
+
+class Print {
+	
+	public static String delimiter = "";
+
+	public static void A() {
+		System.out.println(delimiter);
+		System.out.println("1억줄짜리 코드");
+		System.out.println("1억줄짜리 코드");
+	}
+
+	public static void B() {
+		System.out.println(delimiter);
+		System.out.println("1억줄짜리 코드");
+		System.out.println("1억줄짜리 코드");
+	}
+}
