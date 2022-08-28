bdb6f9522bce9b0c2e6e226e55406d9f966f50c2
seop0728 <ms_sh0728@naver.com>
2022-08-28 19:21:31 +0900

    Algorithm

--- Algorithm/src/beakJoon/Lv_3/Lv3_4.java
+++ Algorithm/src/beakJoon/Lv_3/Lv3_4.java
@@ -27,6 +27,4 @@ public class Lv3_4 {
 
 	    }
 	}
-	}
 
-}
---
+++ Algorithm/src/beakJoon/Lv_4/Lv4_1.java
@@ -0,0 +1,23 @@
+package beakJoon.Lv_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+public class Lv4_1 {
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		int N = in.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+
+		in.close();
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[N - 1]);
+	}
+}
